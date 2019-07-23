ad80d660a0d02188673577cde5ef660e2931f3ea
yamaji.yuichiro <yamaji.yuichiro@trans-cosmos.co.jp>
2019-07-23 18:55:13 +0900

    shift.blade.php改修：途中commit

--- resources/views/contents/shift/shift.blade.php
+++ resources/views/contents/shift/shift.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>BR日報</title>
+	<title>シフト設定</title>
 	<!--CSS -->
 	<link rel="stylesheet" href="./css/bootstrap.min.css" />
 	<!--自作CSS -->
@@ -23,9 +23,9 @@
 </head>
 <body>
 	@env('production')
-	<form method="POST" action="/laravel/report/send">
+	<form method="POST" action="/laravel/shift/send">
 	@else
-	<form method="POST" action="/report/send">
+	<form method="POST" action="/shift/send">
 	@endenv
 	{{ csrf_field() }}
 	<nav>
@@ -38,7 +38,7 @@
 						<option>{{$i}}</option>
 					@endif
 				@endfor
-			</select>/
+			</select> 年 
 			<select name="month" id="01_resize" class="resize">
 				@for($i = 1; $i <= 12; $i++)
 					@if($i == $dates['month'])
@@ -47,41 +47,31 @@
 						<option>{{$i}}</option>
 					@endif
 				@endfor
-			</select>/
-			<select name="_date" id="02_resize" class="resize">
-				@for($i = 1; $i <= 31; $i++)
-					@if($i == $dates['date'])
-						<option selected>{{$i}}</option>
-					@else
-						<option>{{$i}}</option>
-					@endif
-				@endfor
-			</select>
+			</select> 月度
 			<select id="width_tmp_select">
 				<option id="width_tmp_option"></option>
 			</select>
 		</p>
-		<input type="submit" value="日報送信">
-		<p class="float right"><span class="text-danger">残金：</span><span class="remained_cash count" data-num="100">0</span><span class="text-danger">&nbsp円</span></p>
+		<input type="submit" value="表示切替">
+		<!-- <p class="float right"><span class="text-danger">残金：</span><span class="remained_cash count" data-num="100">0</span><span class="text-danger">&nbsp円</span></p> -->
 		<input type="hidden" name="date" value="">
 		<input type="hidden" name="remained_cash">
 	</nav>
 	<div class="container">
 	<main>
 		<section>
-			<div class="section_title">{{$dates['year']}}年{{$dates['month']}}月度シフト</div>
+			<div class="section_title">{{$dates['year']}} 年 {{$dates['month']}} 月度シフト</div>
 			<div class="inner_section">
-				<p class="sm_sec_title">今日の売上げ総額</p>
+				<!-- <p class="sm_sec_title">今日の売上げ総額</p>
 					<p class="form_input">
 						<span id="total_sales" class="total_sales" >0</span>&nbsp円
 						<input type="hidden" name="total_sales" value="">
 					</p>
 				<p class="sm_sec_title">現金売上げ</p>
 					<p class="form_input"><input type="number" class="money sales count plus cash_sales" name="cash_sales" value="">&nbsp円</p>
-					<!-- <p class="form_input">[内：ボトル売上げ]&nbsp&nbsp<input type="number" class="money count plus bottle_sales" name="bottle_sales" step="10" value="">&nbsp円</p>			 -->
 				<p class="sm_sec_title">クレジット売上げ</p>
-					<p class="form_input"><input type="number" class="money sales" name="credit_sales" step="10" value="">&nbsp円</p>
-			</div><!--inner_section-->
+					<p class="form_input"><input type="number" class="money sales" name="credit_sales" step="10" value="">&nbsp円</p> -->
+			</div>
 		</section>		
 	</main>
 	</form>
